7452fd757b4a17f14f3f194a927af18395d9c6a5
hari-hk <hariharakumar5196@gmail.com>
2025-01-30 22:16:43 +0530

    update card titles and images for enhanced product display

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,13 +12,15 @@ export default function Home() {
   const cards = [
     {
       id: 1,
-      title: 'Card 1',
+      title: 'headphone',
       image:
-        'https://lh3.googleusercontent.com/pw/AP1GczNONJuUtISRGs3DW3pGCrVaD7ph0-U9RxmgJItP-WRGTkJtJxM5ujugz0jc9oG5CAPyqN1JoiDZaCBYKbOvOuHQCJMtbnGuWi67U1hDiOQ2Ma-nFoIs=w2400',
+        'https://lh3.googleusercontent.com/pw/AP1GczPoopKVE2b5sMgnOHMLFaS4BZZS6JHZJaIvYDHc-_v2AAk848gV_htKAPSNKMnWm4H4wMAjr599lIT9RMoVXGdjhUZSR9U0ul-9Y2XUXXcyLFaKtg=w2400',
     },
-    { id: 2, title: 'Card 2', image: '/media/22.png' },
-    { id: 3, title: 'Card 3', image: '/media/baby.png' },
-    { id: 4, title: 'Card 4', image: '/media/panasonic.jpg' },
+    { id: 2, title: 'Luggage', image: 'https://lh3.googleusercontent.com/pw/AP1GczMxCwHPRD3zwgwcDiFeI2L4LgPhW5BjfcD9FTDDNQeNUbosdJPxS28euQYovoYxBjZezVW3ePo50YTy8XSnswQHiOmunzXyVppsuNdKIV92epHSoQ=w2400' },
+    { id: 3, title: 'bangle', image: 'https://lh3.googleusercontent.com/pw/AP1GczOPMX-e39C-7lYkbIKr4oP3ifO5AHv6JPj0Gq0bbq9QY0-k0lE4AAcNM1ZQDoh8BsRdpkt6Q70yRck4IC8haYGLdgLKfDAiSFuy6bAml57bWXcvfQ=w2400' },
+    { id: 4, title: 'Mashmelon', image: 'https://lh3.googleusercontent.com/pw/AP1GczPaziCHFQbdIwWKGcpfA7FWZ6fQlPZv26lEIuPsO34bjMGO9IbNRc8Ha4DzyZzGz2p52qSK8lRmSj9yYBGxCy83zp-by5O2KH5q2p1N5PBn89zi0Q=w2400' },
+    { id: 5, title: 'toy', image: 'https://lh3.googleusercontent.com/pw/AP1GczPm7KceH7noqZL3RcEoESdMpDyPRgb-5kzIUEaH_yD5pReh3GJMSrQ9-APxP_D4y4mqOTDJfZKNA6p_w9kTy8Db95NIap93g2LI26KFWYdQhdlYxA=w2400' },
+    { id: 6, title: 'other', image: 'https://lh3.googleusercontent.com/pw/AP1GczP6JgMxC9ODKz1LcbeqbTE7u5qVXnRzEv47VtqQUN_wPmmYKa2rmc3aKz57uyTYkhO0ee8W9sYg2jNZhLBxTQbgQfTx4Rts-C5QObhVkALuSkMgpg=w2400' },
   ];
   return (
     <>
