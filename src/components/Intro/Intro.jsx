d119cf1338f8a01ca61475e215a9f43dbdbbdad9
hari-hk <hariharakumar5196@gmail.com>
2025-02-01 22:39:08 +0530

    add contact page with Google Form integration and enhance Intro component with props for conditional rendering

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,10 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function Intro() {
+export default function Intro({
+  disableDescription = false,
+  hideContactButton = false,
+}) {
   return (
     <>
       <header className='container flex flex-row justify-between items-center '>
@@ -15,24 +18,28 @@ export default function Intro() {
           priority
         />
 
-        <button
-          className='bg-sky-500 text-white px-4 py-4 rounded-lg'
-          onClick={() =>
-            window.open('https://forms.gle/zaesT8XVXkCPn5hUA', '_blank')
-          }
-        >
-          Contact us
-        </button>
+        {!hideContactButton && (
+          <button
+            className='bg-sky-500 text-white px-4 py-4 rounded-lg'
+            onClick={() =>
+              window.open('https://forms.gle/zaesT8XVXkCPn5hUA', '_blank')
+            }
+          >
+            Contact us
+          </button>
+        )}
       </header>
 
-      <main className='container flex flex-col items-center max-w-xl justify-center'>
-        <h1 className='text-5xl text-white font-bold  text-center leading-tight'>
-          Design That Powers Real Business Growth
-        </h1>
-        <p className='text-white text-lg mt-5 text-center'>
-          Elevating brands through innovative and engaging digital solutions
-        </p>
-      </main>
+      {!disableDescription && (
+        <main className='container flex flex-col items-center max-w-xl justify-center'>
+          <h1 className='text-5xl text-white font-bold  text-center leading-tight'>
+            Design That Powers Real Business Growth
+          </h1>
+          <p className='text-white text-lg mt-5 text-center'>
+            Elevating brands through innovative and engaging digital solutions
+          </p>
+        </main>
+      )}
     </>
   );
 }
