31a0a34c8d10a6a74841d6187ed3ab94ba01d368
hari-hk <hariharakumar5196@gmail.com>
2025-02-01 12:13:34 +0530

    add SEO metadata and title to homepage for improved search visibility

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,39 @@
 import Intro from '@/components/Intro';
 import AutoSlider from '@/components/AutoSlider';
 import OurExperience from '@/components/OurExperience';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>
+          Anlyt Media | 3D Modeling, Package Designing & Digital Marketing
+        </title>
+        <meta
+          name='description'
+          content='Anlyt Media specializes in 3D modeling, package designing, and digital marketing, delivering innovative solutions to elevate your brand.'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
+        {/* Open Graph / Facebook */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.anlytmedia.in/' />
+        <meta property='og:title' content='Anlyt Media' />
+        <meta
+          property='og:description'
+          content="Transform your brand with Anlyt Media's expert 3D modeling, package designing, and digital marketing services."
+        />
+
+        {/* Twitter */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://www.anlytmedia.in/' />
+        <meta property='twitter:title' content='Anlyt Media' />
+        <meta
+          property='twitter:description'
+          content="Elevate your brand with Anlyt Media's 3D modeling, package designing & digital marketing services."
+        />
+      </Head>
       <div className=' p-2 bg-black fixed top-0 w-full z-10 '>
         <p className='text-white font-bold  text-center animate-pulse'>
           Website Upgrades in Progress
