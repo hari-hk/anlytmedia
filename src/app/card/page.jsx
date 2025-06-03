1be44e21cf846b4bcc2c1cc25c3d2cc7a26c9f2d
hari-hk <hariharakumar5196@gmail.com>
2025-06-03 11:07:11 +0530

    fix: update pin code

--- src/app/card/page.jsx
+++ src/app/card/page.jsx
@@ -133,7 +133,7 @@ export default function Card() {
                 >
                   <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6' />
                 </svg>
-                18/16/3, Elavuvilai Jn, Marthandam, TN 629191, India
+                18/16/3, Elavuvilai Jn, Marthandam, TN 629171, India
               </li>
             </ul>
           </div>
