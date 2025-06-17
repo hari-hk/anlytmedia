ef6da42eabd16a62bd174ce21939d33d73c672fc
hari hara kumar <hariharakumar5196@gmail.com>
2025-06-17 22:20:39 +0530

    fix: remove unused codes from admin component

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,17 +1,4 @@
-// import api from '@/lib/fetcher';
 import AdminPage from '@/views/Admin/AdminPage';
-// import Login from '@/views/Login/Login';
-// import { getServerSideProps } from 'next/dist/build/templates/pages';
-// import { use } from 'react';
-
-// function Admin() {
-//   // const response = use(await api.get('/store'));
-//   // console.log('Store data:', response);
-//   return  <Login />;
-//   // return <AdminPage />;
-// }
-
-// export default Admin;
 export default function Admin() {
   return <AdminPage />;
 }
