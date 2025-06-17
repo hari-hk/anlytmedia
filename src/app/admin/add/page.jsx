77ea8ed05dd5abf2c76c52179d68f5c79757bbb5
hari hara kumar <hariharakumar5196@gmail.com>
2025-06-17 21:54:54 +0530

    remove api

--- src/app/admin/add/page.jsx
+++ src/app/admin/add/page.jsx
@@ -1,7 +1,6 @@
 import BusinessCardForm from '@/views/Admin/BusinessCardForm';
 
 async function AddBusinessCard() {
-  // const response = await fetch('/api/store');
   return <BusinessCardForm />;
 }
 
