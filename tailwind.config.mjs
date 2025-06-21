3224c9f8735c045c9d5a57c5d3e6ab3177a055b5
hari hara kumar <hariharakumar5196@gmail.com>
2025-06-21 12:06:27 +0530

    feat: whitelist colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/views/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
+  safelist: [
+    {
+      pattern:
+        /^(bg|text|border|from|to|via)-(red|green|blue|yellow|purple|pink|indigo|gray|slate|neutral|zinc|stone|amber|orange|lime|emerald|teal|cyan|sky|violet|fuchsia|rose)-(50|100|200|300|400|500|600|700|800|900)$/,
+    },
+    {
+      pattern: /^(bg|text|border|from|to|via)-(black|white)$/,
+    },
+    {
+      pattern: /^bg-gradient-to-(r|l|t|b|tr|tl|br|bl)$/,
+    },
+  ],
   plugins: [],
 };
